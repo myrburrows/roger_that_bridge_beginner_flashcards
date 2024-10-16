a0faf683a6495d0fc318c0a192b8eaa8155f432a
myrburrows <90137418+myrburrows@users.noreply.github.com>
2024-10-16 17:35:27 -0500

    add scoring deck

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let serverAvailable = false; // Keep track of server availability
 
     // Hardcoded list of flashcard file names without extensions
-    const files = ['Basics','Beginning Bidding']; // Add new filenames here as needed
+    const files = ['Basics','Beginning Bidding','Beginning Play','Bridge Scoring']; // Add new filenames here as needed
 
     // Immediately check if the server is available before proceeding
     checkServerAvailability();
